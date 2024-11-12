6824e4cd685c2b4f28d2469aaebe435173747409
Johnny Dutra <johnny.dutra@gmail.com>
2024-11-12 11:20:57 -0300

    section 5: integrating rabbitmq

--- cypress/e2e/tasks/post.cy.js
+++ cypress/e2e/tasks/post.cy.js
@@ -5,28 +5,50 @@ describe('POST /tasks', () => {
     });
   });
 
-  it('should register a new task', function () {
-    const { user, task } = this.tasks.create;
+  context('should register a new task', function () {
 
-    cy.task('removeUser', user.email);
-    cy.postUser(user);
+    before(function () {
+      cy.purgeQueueMessages()
+        .then(response => {
+          expect(response.status).to.eq(204);
+        });
+    });
 
-    cy.postSession(user)
-      .then(sessionResp => {
-        cy.task('removeTask', task.name, user.email);
-        cy.postTask(sessionResp.body.token, task)
-          .then(response => {
-            expect(response.status).to.eq(201);
-            expect(response.body.name).to.eq(task.name);
-            expect(response.body.tags).to.eql(task.tags);
-            expect(response.body.is_done).to.be.false;
-            expect(response.body.user).to.eq(sessionResp.body.user._id);
-            expect(response.body._id.length).to.eq(24);
-          });
-      });
+    it('post task', function () {
+      const { user, task } = this.tasks.create;
+  
+      cy.task('removeUser', user.email);
+      cy.postUser(user);
+  
+      cy.postSession(user)
+        .then(sessionResp => {
+          cy.task('removeTask', task.name, user.email);
+          cy.postTask(sessionResp.body.token, task)
+            .then(response => {
+              expect(response.status).to.eq(201);
+              expect(response.body.name).to.eq(task.name);
+              expect(response.body.tags).to.eql(task.tags);
+              expect(response.body.is_done).to.be.false;
+              expect(response.body.user).to.eq(sessionResp.body.user._id);
+              expect(response.body._id.length).to.eq(24);
+            });
+        });
+    });
+
+    after(function () {
+      const { user, task } = this.tasks.create;
+      cy.wait(3000);
+      cy.getMessageQueue()
+        .then(response => {
+          expect(response.status).to.eq(200);
+          expect(response.body[1].payload).to.include(user.name.split(' ')[0]);
+          expect(response.body[1].payload).to.include(task.name);
+          expect(response.body[1].payload).to.include(user.email);
+        });
+    });
   });
 
-  it('should not register a duplicated task', function () {
+  it.skip('should not register a duplicated task', function () {
     const { user, task } = this.tasks.duplicated;
 
     cy.task('removeUser', user.email);
